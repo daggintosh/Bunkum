5197849e38c06350b86e44244a9ba9dba578b592
Dagg <dagg@kyzune.com>
2023-06-15 16:26:33 -0700

    Reduce thread count for tests to avoid long test times and hanging of low memory systems

--- BunkumTests.HttpServer/ServerDependentTest.cs
+++ BunkumTests.HttpServer/ServerDependentTest.cs
@@ -5,6 +5,11 @@ using JetBrains.Annotations;
 
 namespace BunkumTests.HttpServer;
 
+/*
+ * Too much parallelization causes trouble, we don't need 220 threads trying to figure out if Dummy is authed or not
+ * In the future, this should determine the amount of viable threads for doing tests,
+ * we need to fit the tests into a more sane amount of threads
+*/
 [Parallelizable]
 public class ServerDependentTest
 {
@@ -16,7 +21,7 @@ public class ServerDependentTest
 
         BunkumHttpServer server = new(listener);
         server.AddAuthenticationService();
-        if(start) server.Start();
+        if(start) server.Start(1);
 
         return (server, client);
     }
